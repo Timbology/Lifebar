dc21ca0f8797348f9b71cc8d0b94333669eccf6a
Timbology <i7679245@bournemouth.ac.uk>
2018-04-30 14:59:58 +0100

    Lifebar
    Log in / Sign Up segue's connected with Firebase. Need to create main screen.

---
+++ Lifebar/LISU/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Lifebar
+//
+//  Created by Timbology on 30/04/2018.
+//  Copyright © 2018 Timbology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleLogout(_ sender:Any) {
+        try! Auth.auth().signOut()
+        self.dismiss(animated: false, completion: nil)
+    }
+}
